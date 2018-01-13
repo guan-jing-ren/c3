e3827bb9197f5ef0704076a9911b4303eb32dc8c
guan-jing-ren <kwan.king.yan@gmail.com>
2018-01-13 19:38:01 +1100

    Test data factory function wrappers. Change from node to element.

--- c3.cpp
+++ c3.cpp
@@ -21,6 +21,31 @@ using namespace std;
 
 struct Transition {};
 
+void wrapped();
+void wrapped0(int);
+void wrapped1(Element &);
+void wrapped2(int, Element &);
+void wrapped3(Element &, int);
+void wrapped4(int, const vector<Element> &, Element &);
+void wrapped5(int, Element &, const vector<Element> &);
+void wrapped6(Element &, int, const vector<Element> &);
+void wrapped7(Element &, const vector<Element> &, int);
+void wrapped8(const vector<Element> &, int, Element &);
+void wrapped9(const vector<Element> &, Element &, int);
+void unwrapped(string);
+
+static_assert(is_detected_v<wrappable, int, decltype(wrapped)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped0)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped1)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped2)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped3)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped4)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped5)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped6)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped7)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped8)>);
+static_assert(is_detected_v<wrappable, int, decltype(wrapped9)>);
+
 int main() {
   clog << "c3 loaded.\n";
 
@@ -34,7 +59,7 @@ int main() {
   for (auto &&child : doc.body().children())
     cout << child.nodeName() << ": " << child.textContent() << "\n";
 
-  c3().selectAll("p").forEach([](const Node &n) {
+  c3().selectAll("p").forEach([](const Element &n) {
     cout << n.nodeName() << ": " << n.textContent() << "\n";
   });
 
